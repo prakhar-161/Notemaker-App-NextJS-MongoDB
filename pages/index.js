64c8ccefc4da828e2021a4ffff77f944b7ae41ad
Prakhar Dhama <pdhama161@gmail.com>
2022-02-02 08:01:25 +0530

    done index page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
-import Head from 'next/head';
+import Link from "next/link";
+import fetch from 'isomorphic-unfetch';
+import { Button, Card } from 'semantic-ui-react';
 
-
-export default function Home() {
+const Index = ({ notes }) => {
   return (
-    <div>
-      
+    <div className="notes-container">
+      <h1>Notes</h1>
+      <div className="grid wrapper">
+        {notes.map((note) => (
+          <div key={note._id}>
+            <Card>
+              <Card.Content>
+                <Card.Header>
+                  <Link href={`/${note._id}`}>
+                    <a>{note.title}</a>
+                  </Link>
+                </Card.Header>
+              </Card.Content>
+              <Card.Content extra>
+                <Link href={`/${note._id}`}>
+                  <Button primary>View</Button>
+                </Link>
+                <Link href={`/${note._id}/edit`}>
+                  <Button primary>Edit</Button>
+                </Link>
+              </Card.Content>
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+Index.getInitialProps = async () => {
+  const res = await fetch('http://localhost:3000/api/notes');
+  const { data } = await res.json();
+
+  return { notes: data };
+};
+
+export default Index;
